a8982705eb56a7e8765ecccce92121a93c732c34
Andreas Hartel <andreas@hartel.me>
2025-11-06 20:12:20 +0100

    train can drive

---
+++ README.md
@@ -0,0 +1,8 @@
+# trainwaves
+
+A train ~~tower~~ station defense game.
+Trains come in waves and need to be steered into their desired stations.
+
+The game is for the Godot game engine and relies heavily on the great project
+[simple-godot-train](https://github.com/moonbench/simple-godot-train) by
+[moonbench](https://github.com/moonbench/simple-godot-train).
---
+++ Scripts/game.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+@export var car_count = 8
+@export var train_vehicle_reference : PackedScene
+
+@onready var engine : TrainEngine = $TrainEngine
+
+func _setup_train():
+	engine.add_to_track($Tracks/Track)
+	
+	var last_car = engine
+	for index in range(car_count):
+		var car = train_vehicle_reference.instantiate()
+		add_child(car)
+		last_car.set_follower_car(car)
+		last_car = car
+
+func _on_timer_timeout() -> void:
+	_setup_train()
