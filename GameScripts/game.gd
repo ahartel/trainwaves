4c728658ea8a89de96aa88564e339d078aeb5a78
Andreas Hartel <andreas@hartel.me>
2025-11-14 08:45:09 +0100

    lighter train with shorter start/stop times

--- GameScripts/game.gd
+++ GameScripts/game.gd
@@ -9,10 +9,14 @@ func _setup_train():
 	engine.add_to_track($Tracks/Track)
 	engine.velocity = 2.0
 	engine.target_force_percent = 0.4
+	engine.mass = 1.0
+	engine.change_towed_mass(0)
 	
 	var last_car = engine
 	for index in range(car_count):
 		var car = train_vehicle_reference.instantiate()
+		car.mass = 1.0
+		car.change_towed_mass(0)
 		add_child(car)
 		last_car.set_follower_car(car)
 		last_car = car
